f081230ea67550272c4e84a7a39e21697bc4ddc4
Himanshi Jangid <himanshijangid444@gmail.com>
2023-02-28 17:56:08 +0530

    hj

---
+++ script.js
@@ -0,0 +1,24 @@
+const groceries = document.getElementsByClassName("groceries");
+const pencil = document.getElementById("pencil");
+const allItems = document.getElementById("allItems");
+const userInput = document.getElementById("userInput");
+
+pencil.addEventListener("click", () => {
+    allItems.innerHTML = "";
+})
+userInput.addEventListener("keydown" ,(event) => {
+    if(event.key=="Enter"){
+        addItem();
+    }
+})
+function addItem(){
+    let h2 = document.createElement("h2");
+    h2.innerHTML = "- " +userInput.value;
+
+    h2.addEventListener("click",()=> {
+        h2.style.textDecoration = "line-through";
+
+    })
+    allItems.insertAdjacentElement("beforeend",h2);
+    userInput.value = "";
+}
